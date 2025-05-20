6f4524501ffb8fbba117414332e6684fd2fe42e0
JP <jpniznhy@hotmail.com>
2025-05-20 17:17:13 +0200

    Add the pegasus model

--- src/model_loader.py
+++ src/model_loader.py
@@ -1,4 +1,9 @@
-from transformers import BartTokenizer, BartForConditionalGeneration
+from transformers import (
+    BartTokenizer,
+    BartForConditionalGeneration,
+    PegasusTokenizer,
+    PegasusForConditionalGeneration,
+)
 
 
 def load_bart_model(model_name="facebook/bart-base"):
@@ -12,4 +17,18 @@ def load_bart_model(model_name="facebook/bart-base"):
     """
     tokenizer = BartTokenizer.from_pretrained(model_name)
     model = BartForConditionalGeneration.from_pretrained(model_name)
+    return tokenizer, model
+
+
+def load_pegasus_model(model_name="google/pegasus-xsum"):
+    """
+    Load a Pegasus model and tokenizer from the Hugging Face model hub.
+    Args:
+        model_name (str): The name of the Pegasus model to load. Default is 'google/pegasus-xsum'.
+    Returns:
+        tokenizer: The tokenizer for the Pegasus model.
+        model: The Pegasus model.
+    """
+    tokenizer = PegasusTokenizer.from_pretrained(model_name)
+    model = PegasusForConditionalGeneration.from_pretrained(model_name)
     return tokenizer, model
